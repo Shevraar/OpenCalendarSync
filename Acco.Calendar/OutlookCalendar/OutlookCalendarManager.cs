ee1d6a875135d589dee5d1a58f6d3b7c54f3dd80
Marco Accorinti <accosk8@gmail.com>
2014-07-03 22:03:34 +0200

    -- Refactoring, moved specific managers to their folders.

---
+++ Acco.Calendar/OutlookCalendar/OutlookCalendarManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acco.Calendar.Manager
+{
+
+    public class OutlookCalendarManager : ICalendarManager
+    {
+        public bool Push(GenericCalendar calendar)
+        {
+            return false;
+        }
+
+        public GenericCalendar Pull()
+        {
+            return null;
+        }
+
+        public async Task<bool> PushAsync(GenericCalendar calendar)
+        {
+            return false;
+        }
+
+        public async Task<GenericCalendar> PullAsync()
+        {
+            return null;
+        }
+    }
+}
