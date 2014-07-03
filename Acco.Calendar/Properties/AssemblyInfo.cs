81b57c53754979045543b0e5672fc673c304e2fa
Marco Accorinti <accosk8@gmail.com>
2014-07-03 22:07:04 +0200

    -- AssemblyVersion auto-increment

--- Acco.Calendar/Properties/AssemblyInfo.cs
+++ Acco.Calendar/Properties/AssemblyInfo.cs
@@ -34,5 +34,4 @@ using System.Resources;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.1.*")]
-[assembly: AssemblyFileVersion("0.1.*")]
 [assembly: NeutralResourcesLanguageAttribute("en")]
