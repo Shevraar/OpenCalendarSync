9390ce6aa6a2d45120f38c4b3cb8917e96c63442
Marco Accorinti <accosk8@gmail.com>
2014-10-24 20:12:33 +0200

    Fuck

---
+++ OpenCalendarSync.Lib/Person.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using OpenCalendarSync.Lib.Utilities;
+using OutlookResponseStatusEnum = Microsoft.Office.Interop.Outlook.OlResponseStatus;
+
+namespace OpenCalendarSync.Lib.Person
+{
+    public interface IPerson
+    {
+        string Email { get; set; }
+
+        string Name { get; set; }
+
+        string FirstName { get; set; }
+
+        string LastName { get; set; }
+    }
+
+    public class GenericPerson : IPerson
+    {
+        [RegularExpression(Defines.EmailRegularExpression,
+                            ErrorMessage = "Not a valid email address")]
+        public string Email { get; set; }
+
+        public string Name { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            // If parameter is null return false.
+            if (obj == null)
+            {
+                return false;
+            }
+
+            // If parameter cannot be cast to Point return false.
+            var p = obj as GenericPerson;
+            if ((object)p == null)
+            {
+                return false;
+            }
+
+            return  (this.Email == p.Email) &&
+                    (this.Name == p.Name) &&
+                    (this.FirstName == p.FirstName) &&
+                    (this.LastName == p.LastName);
+        }
+
+        public override int GetHashCode()
+        {
+            return this.Email.GetHashCode();
+        }
+    }
+
+    class GoogleResponseStatus : Attribute
+    {
+        public string Text { get; private set; }
+
+        public GoogleResponseStatus(string text)
+        {
+            Text = text;
+        }
+    }
+
+    class OutlookResponseStatus : Attribute
+    {
+        public Microsoft.Office.Interop.Outlook.OlResponseStatus OlResponse { get; private set; }
+
+        public OutlookResponseStatus(Microsoft.Office.Interop.Outlook.OlResponseStatus olResponse)
+        {
+            OlResponse = olResponse;
+        }
+    }
+
+    public enum ResponseStatus : ushort
+    {
+        [GoogleResponseStatus("accepted"),
+        OutlookResponseStatus(OutlookResponseStatusEnum.olResponseAccepted)]
+        Accepted = 0,
+
+        [GoogleResponseStatus("declined"),
+        OutlookResponseStatus(OutlookResponseStatusEnum.olResponseDeclined)]
+        Declined,
+
+        [GoogleResponseStatus("tentative"),
+        OutlookResponseStatus(OutlookResponseStatusEnum.olResponseTentative)]
+        Tentative,
+
+        [GoogleResponseStatus("accepted"),
+        OutlookResponseStatus(OutlookResponseStatusEnum.olResponseOrganized)]
+        Organized,
+
+        [GoogleResponseStatus("needsAction"),
+        OutlookResponseStatus(OutlookResponseStatusEnum.olResponseNone)]
+        None,
+
+        [GoogleResponseStatus("needsAction"),
+        OutlookResponseStatus(OutlookResponseStatusEnum.olResponseNotResponded)]
+        NotResponded
+    }
+
+    /// <summary>
+    /// Specialization of GenericPerson, with ResponseStatus as an additional parameter
+    /// </summary>
+    public class GenericAttendee : GenericPerson
+    {
+        /// <summary>
+        /// The response status of the meeting/event/appointment attendee
+        /// </summary>
+        public ResponseStatus Response { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            // If parameter is null return false.
+            if (obj == null)
+            {
+                return false;
+            }
+
+            // If parameter cannot be cast to Point return false.
+            var p = obj as GenericAttendee;
+            if ((object)p == null)
+            {
+                return false;
+            }
+
+            return (base.Equals(p)) &&
+                   (this.Response == p.Response);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+    }
+}
---
+++ OpenCalendarSync.Lib/Utilities/Utilities.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using WallF.BaseNEncodings;
+
+namespace OpenCalendarSync.Lib.Utilities
+{
+    public static class Defines
+    {
+        public const string EmailRegularExpression = @"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?";
+    }
+
+    public static class EnumHelper
+    {
+        /// <summary>
+        /// Gets an attribute on an enum field value
+        /// </summary>
+        /// <typeparam name="T">The type of the attribute you want to retrieve</typeparam>
+        /// <param name="enumVal">The enum value</param>
+        /// <returns>The attribute of type T that exists on the enum value</returns>
+        public static T GetAttributeOfType<T>(this Enum enumVal) where T : Attribute
+        {
+            var type = enumVal.GetType();
+            var memInfo = type.GetMember(enumVal.ToString());
+            var attributes = memInfo[0].GetCustomAttributes(typeof(T), false);
+            return (attributes.Length > 0) ? (T)attributes[0] : null;
+        }
+    }
+
+    public static class StringHelper
+    {
+        public static byte[] GetBytes(string str)
+        {
+            var bytes = new byte[str.Length * sizeof(char)];
+            Buffer.BlockCopy(str.ToCharArray(), 0, bytes, 0, bytes.Length);
+            return bytes;
+        }
+
+        public static string GetString(byte[] bytes)
+        {
+            var chars = new char[bytes.Length / sizeof(char)];
+            Buffer.BlockCopy(bytes, 0, chars, 0, bytes.Length);
+            return new string(chars);
+        }
+
+        private static readonly char[] Alphabet = "0123456789abcdefghijklmnopqrstuv".ToCharArray();
+        private const char Padding = '\0';
+        private static readonly BaseEncoding googleBase32 = new Base32Encoding(Alphabet, Padding, "GoogleBase32Enconding");
+
+        public static BaseEncoding GoogleBase32
+        {
+            get { return googleBase32; }
+        }
+    }
+
+    public static class MethodHelper
+    {
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public static string GetCurrentMethod()
+        {
+            StackTrace st = new StackTrace();
+            StackFrame sf = st.GetFrame(1);
+
+            return sf.GetMethod().Name;
+        }
+    }
+
+    public static class VersionHelper
+    {
+        public static string GetCurrentVersion()
+        {
+            return System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString();
+        }
+    }
+}
